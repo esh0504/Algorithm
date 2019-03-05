c5c315e0abd1abc20d6f6353735ed5a97b090018
음승호 <dmatmdgh49@gmail.com>
2019-03-06 07:21:36 +0900

    에라토스테네스의 체 구현

--- practice/1929.py
+++ practice/1929.py
@@ -4,33 +4,35 @@ m,n=map(int,input().split())
 
 num=int(math.sqrt(n))
 
+list=[]
+listcopy=[]
 a=[]
 acopy=[]
-d=[]
-dcopy=[]
 answer=[]
+
 for i in range(m,n+1):
-    a.append(i)
+    list.append(i)
 
 for i in range(2,num+1):
-    d.append(i)
+    a.append(i)
 
-while(len(d)!=0):
-    dcopy=[]
+while (len(a)!=0):
+    answer.append(a[0])
+    acopy=[]
+    listcopy=[]
+    for i in range(len(list)):
+        if list[i]%a[0]!=0:
+            listcopy.append(list[i])
     for i in range(len(a)):
-        if a[i]%d[0]!=0:
+        if a[i]%a[0]!=0:
             acopy.append(a[i])
-        else:
-            continue
-    for i in range(len(d)):
-        answer.append(d[i])
-        if d[i]%d[0]!=0:
-            dcopy.append(d[i])
-        else:
-            continue
 
+    list=listcopy
     a=acopy
-    d=dcopy
 
 for i in range(len(answer)):
-    print(answer[i])
+    if answer[i]>=m:
+        print(answer[i])
+for i in range(len(list)):
+    if list[i]!=1:
+        print(list[i])
