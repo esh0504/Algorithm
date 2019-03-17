7349b41ad6399d516f5e45735c100b7f5189f678
승호 음 <dmatmdgh49@gmail.com>
2019-03-18 07:03:30 +0900

    mission complete

--- practice/1157.py
+++ practice/1157.py
@@ -7,23 +7,25 @@ for i in range(len(word)):
         wordOrd.append(ord(word[i]))
 wordOrd.sort()
 count=0
-count2=0
+count2=1
 answer=word[0]
-print(wordOrd)
 wordOrd.append(10000)
+for i in range(len(wordOrd)-1):
+    wordOrd[i]=chr(wordOrd[i])
+
 for i in range(len(word)):
-    count2+=1
+
     if wordOrd[i]==wordOrd[i+1]:
         count2+=1
     else:
         if count>count2:
-            count2=0
-            answer=chr(wordOrd[i])
+            count2=1
         elif count==count2:
-            count2=0
+            count2=1
             answer='?'
         else:
             count=count2
-            count2=0
-            answer=chr(wordOrd[i])
+            count2=1
+            answer=wordOrd[i]
+
 print(answer)
