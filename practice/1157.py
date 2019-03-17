fab1988fcf8eb5932e1b86f27558ed413ddeb5c5
음승호 <dmatmdgh49@gmail.com>
2019-03-17 19:55:29 +0900

    1157 Noncomplete

---
+++ practice/1157.py
@@ -0,0 +1,27 @@
+word=input()
+wordOrd=[]
+for i in range(len(word)):
+    if ord(word[i])>=97:
+        wordOrd.append(ord(word[i])-32)
+    else:
+        wordOrd.append(ord(word[i]))
+wordOrd.sort()
+count=0
+count2=0
+answer=word[0]
+print(wordOrd)
+for i in range(len(word)-1):
+    count2+=1
+    if wordOrd[i]==wordOrd[i+1]:
+        count2+=1
+    else:
+        if count>count2:
+            count2=0
+        elif count==count2:
+            count2=0
+            answer='?'
+        else:
+            count=count2
+            count2=0
+            answer=word[i]
+print(answer)
